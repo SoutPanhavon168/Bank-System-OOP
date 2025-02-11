90b7faeb2804cc148a204da52097e4a957de8bda
sou1150 <prumsereyreaksa@gmail.com>
2025-02-11 13:50:51 +0700

    Folders

---
+++ Authenication.java
@@ -0,0 +1,7 @@
+public interface Authenication {
+    public boolean login();
+    public boolean register();
+    public boolean logout();
+    public boolean changePassword();
+    public boolean forgotPassword();
+}
