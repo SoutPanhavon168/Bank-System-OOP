2437f9e3a16407a36b95c7890235ac25d913db14
Heng <bunheng.try@student.cadt.edu.kh>
2025-02-12 11:38:42 +0700

    Field admin class done

---
+++ src/Admin/admin.java
@@ -0,0 +1,14 @@
+package Admin;
+
+public class admin {
+    private String email;
+    private String password;
+    public String role;    
+
+    public admin(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    
+}
