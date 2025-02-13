bbbfbfc81ed1a0db967623e2bfec12bc4fdb0bc3
Heng <bunheng.try@student.cadt.edu.kh>
2025-02-13 22:42:09 +0700

    Done with 3 interface

--- src/admin/admin.java
+++ src/admin/admin.java
@@ -1,14 +1,63 @@
 package Admin;
+import java.util.Scanner;
+import Interfaces.Authentication;
 
-public class admin {
-    private String email;
+public class Admin implements Authentication {
+    private String firstName;
+    private String lastName;    
     private String password;
-    public String role;    
 
-    public admin(String email, String password, String role) {
-        this.email = email;
+    public Admin(String firstname, String lastname , String password, String role) {
+        this.firstName = firstname;
+        this.lastName = lastname;
         this.password = password;
-        this.role = role;
     }
+    @Override
+    public boolean login(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your firstname: ");
+            String InputFirstname = scanner.nextLine();
+            System.out.println("Enter your lastname: ");
+            String InputLastname = scanner.nextLine();
+            System.out.println("Enter your password: ");
+            String InputPassword = scanner.nextLine();
+            if (this.firstName.equals(InputFirstname) && this.lastName.equals(InputLastname) && this.password.equals(InputPassword)) {   
+                return true;
+            }else {
+                return false;
+            }
+        }
+        @Override
+        public boolean register() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your firstname: ");
+            String InputFirstname = scanner.nextLine();
+            System.out.println("Enter your lastname: ");
+            String InputLastname = scanner.nextLine();
+            System.out.println("Enter your password: ");
+            String InputPassword = scanner.nextLine();
     
-}
+            System.out.println("New user registered:  " + InputFirstname + " " + InputLastname);
+            this.firstName = InputFirstname;
+            this.lastName = InputLastname; 
+            this.password = InputPassword;
+            //write to file
+            return true;
+        }
+        public boolean forgotPassword() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your firstname: ");
+            String InputFirstname = scanner.nextLine();
+            System.out.println("Enter your lastname: ");
+            String InputLastname = scanner.nextLine();
+            if (this.firstName.equals(InputFirstname) && this.lastName.equals(InputLastname)) {
+                System.out.println("Enter your new password: ");
+                String Input_newPassword = scanner.nextLine();
+                this.password = Input_newPassword;
+                System.out.println("Password updated successfully.");
+            } else {
+                System.out.println("User not found.");
+            }
+            return true;
+        }
+    }
