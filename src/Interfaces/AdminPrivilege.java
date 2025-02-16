d4256e5d996bf5b34c1294b4903ef587ba4ecce9
Heng <bunheng.try@student.cadt.edu.kh>
2025-02-16 23:05:50 +0700

    Change

--- src/Interfaces/AdminPrivilege.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-public interface AdminPrivilege extends Management {
-    void addAccount();  // Only Admin
-    void removeAccount();  // Only Admin
-    boolean approveLargeLoan(int loanId);  // Only Admin
-    void viewAllTransactions();  // Only Admin
-    void viewAllPayments();  // Only Admin
-}
