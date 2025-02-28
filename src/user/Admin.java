d20afe91b1e543d56e84872560004b0a71671cb6
Heng <bunheng.try@student.cadt.edu.kh>
2025-02-28 11:31:34 +0700

    Changed

--- src/user/Admin.java
+++ src/user/Admin.java
@@ -1,38 +1,111 @@
 package user;
-import java.util.Scanner;
-import Interfaces.Management;
 
+import java.time.LocalDate;
+import java.util.Scanner;
 //admin needs to login first before doing any action
-public class Admin extends User  {
+public class Admin extends User {
     private String admin_username;
     private String admin_password; 
 
-
-    public Admin(String admin_password, String admin_username, int userId, String firstName, String lastName, String phoneNumber, String password, String confirmPassword, String email, String birthDate, String governmentId, String userType) {
-        super(userId, firstName, lastName, phoneNumber, password, confirmPassword, email, birthDate, governmentId, userType);
+    public Admin(String admin_password,String admin_username,String lastName, String firstName, String email, String password, String confirmPassword,
+            String phoneNumber, LocalDate birthDate, String governmentId) {
+        super(lastName, firstName, email, password, confirmPassword, phoneNumber, birthDate, governmentId);
         this.admin_username = admin_username;
-        this.admin_password = admin_password;
+        this.admin_password = admin_password; 
+    }
+
+    public void admin_login(){
+        Scanner sc = new Scanner(System.in);
+        try  {
+            System.out.println("Enter the admin username: ");
+            String username = sc.nextLine();
+            System.out.println("Enter the admin password: ");
+            String password = sc.nextLine();
+            if(username.equals(this.admin_username) && password.equals(this.admin_password)){
+                System.out.println("========Admin Menu=======");
+                System.out.println("1. Add Account");
+                System.out.println("2. Remove Account");
+                System.out.println("3. Update Account");
+                System.out.println("4. Approve Large Loan");
+                System.out.println("5. View All Transactions");
+                System.out.println("6. View All Payments");
+                System.out.println("Please choose an option (1-6): ");
+                int choice = sc.nextInt();
+                sc.nextLine();
+                switch (choice) {
+                    case 1:
+                        addAccount();
+                        break;
+                    case 2:
+                        removeAccount();
+                        break;
+                    case 3:
+                        updateAccount();
+                        break;
+                    case 4:
+                        approveLargeLoan(1);
+                        break;
+                    case 5:
+                        viewAllTransactions();
+                        break;
+                    case 6:
+                        viewAllPayments();
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
+            }else{
+                System.out.println("Invalid username or password");
+            }
+        } catch (Exception e) {
+            System.out.println("Error!!!");
+        } finally {
+            sc.close();
+        }
     }
     public void addAccount() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first name: ");
-        String firstName = sc.nextLine();
-        System.out.println("Enter the last name: ");
-        String lastName = sc.nextLine();
-        System.out.println("Enter the phone number: ");
-        String phoneNumber = sc.nextLine();
-        System.out.println("Enter the password: ");
-        String password = sc.nextLine();
-        System.out.println("Enter the confirm password: ");
-        String confirmPassword = sc.nextLine();
-        System.out.println("Enter the email: ");
-        String email = sc.nextLine();
-        System.out.println("Enter the birth date: ");
-        String birthDate = sc.nextLine();
-        System.out.println("Enter the government ID: ");
-        String governmentId = sc.nextLine();
-        System.out.println("Enter the user type: ");
-        String userType = sc.nextLine();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the first name: ");
+            String firstName = sc.nextLine();
+            System.out.println("Enter the last name: ");
+            String lastName = sc.nextLine();
+            System.out.println("Enter the phone number: ");
+            String phoneNumber = sc.nextLine();
+            if(!isPhoneNumberValid(phoneNumber)){
+                System.out.println("Invalid phone number. Please enter a valid phone number: ");
+                phoneNumber = sc.nextLine();
+            }
+            System.out.println("Enter the email: ");
+            String email = sc.nextLine();
+            while(!isEmailValid(email)){
+                System.out.println("Invalid email. Please enter a valid email: ");
+                email = sc.nextLine();
+            }
+            System.out.println("Enter the password: ");
+            String password = sc.nextLine();
+            System.out.println("Enter the confirm password: ");
+            String confirmPassword = sc.nextLine();
+            do {
+                System.out.print("Confirm your password: ");
+                confirmPassword = sc.nextLine();
+                if (!confirmPassword.equals(password)) {
+                    System.out.println("Passwords do not match. Try again.");
+                }
+            }while (!confirmPassword.equals(password));
+            System.out.println("Enter the birth date: ");
+            String birthDate = sc.nextLine();
+            do {
+                System.out.println("Enter the government ID: ");
+                String governmentId = sc.nextLine();
+                if(!isGovernmentIdValid(governmentId)){
+                    System.out.println("Invalid government ID. Must be at least 6 characters long.");
+                    governmentId = sc.nextLine();
+                }
+            } while (!isGovernmentIdValid(governmentId));
+            System.out.println("Enter the user type: ");
+            String userType = sc.nextLine();
+        }
 
         //wait todo until we have the database
 
